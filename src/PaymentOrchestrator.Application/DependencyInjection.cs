504c2b4ce8ddc7c57ba3c84f6a8e6482d15217ec
Simon Holmquist <simonholmquist@outlook.com>
2026-01-13 18:14:06 -0300

    chore(app): configure application layer dependency injection

---
+++ src/PaymentOrchestrator.Application/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentOrchestrator.Application.Common.Behaviors;
+
+namespace PaymentOrchestrator.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Registrar MediatR
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+
+            // Registrar comportamientos del pipeline en orden
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+        });
+
+        // Registrar validadores de FluentValidation
+        services.AddValidatorsFromAssembly(assembly);
+
+        return services;
+    }
+}
