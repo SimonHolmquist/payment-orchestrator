06c3a353f57103efd420f1fafc13e3056e6b59e8
Simon Holmquist <simonholmquist@outlook.com>
2026-01-13 18:53:29 -0300

    chore(infra): register infrastructure services and db connection

--- src/PaymentOrchestrator.Api/Program.cs
+++ src/PaymentOrchestrator.Api/Program.cs
@@ -1,9 +1,14 @@
+using PaymentOrchestrator.Application;
+using PaymentOrchestrator.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/PaymentOrchestrator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PaymentOrchestrator.Application.Common.Interfaces;
+using PaymentOrchestrator.Infrastructure.Persistence;
+using PaymentOrchestrator.Infrastructure.Persistence.Interceptors;
+using PaymentOrchestrator.Infrastructure.Persistence.Repositories;
+
+namespace PaymentOrchestrator.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("SqlServer");
+
+        services.AddSingleton<DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<PaymentOrchestratorDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetRequiredService<DispatchDomainEventsInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IPaymentRepository, EfPaymentRepository>();
+        services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+
+        return services;
+    }
+}
