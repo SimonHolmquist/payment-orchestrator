b6fc6db9c6291e57403f9c358cfb553bdf560163
Simon Holmquist <simonholmquist@outlook.com>
2026-01-13 23:14:32 -0300

    IPspClient added

---
+++ src/PaymentOrchestrator.Application/Common/Interfaces/IPspClient.cs
@@ -0,0 +1,8 @@
+﻿// src/PaymentOrchestrator.Application/Common/Interfaces/IPspClient.cs
+namespace PaymentOrchestrator.Application.Common.Interfaces;
+
+public interface IPspClient
+{
+    // Solo definimos el contrato base por ahora
+    Task<bool> AuthorizeAsync(string paymentId, decimal amount, string currency, CancellationToken ct);
+}
